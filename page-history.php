f9cb6b54753c9bb9f0217bc3f16866834b86292c
Vladislav Svitkovskiy <svitkovskiy198@gmail.com>
2021-03-17 21:16:54 +0300

    added new pages of website

---
+++ page-history.php
@@ -0,0 +1,84 @@
+<?php
+/*
+Template Name: Наша история
+*/
+?>
+
+<?php
+get_heager();
+?>
+
+<div class="aboutus">
+    <div class="container">
+        <h1 class="title"><?php the_field('aboutus_title'); ?></h1>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="subtitle">
+                    <?php the_field('aboutus_title_1'); ?>
+                </div>
+                <div class="aboutus__text">
+                    <?php the_field('aboutus_descr_1'); ?>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <?php // подставляем изображение (вариант с массивом) из группы полей
+                $image = get_field('aboutus_img_1');
+
+                if (!empty($image)): ?>
+                    <img class="aboutus__img"
+                         src="<?php echo $image['url']; ?>"
+                         alt="<?php echo $image['alt']; ?>">
+                <?php endif;
+                ?>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-6">
+                <?php // подставляем изображение (вариант с массивом) из группы полей
+                $image = get_field('aboutus_img_2');
+
+                if (!empty($image)): ?>
+                    <img class="aboutus__img"
+                         src="<?php echo $image['url']; ?>"
+                         alt="<?php echo $image['alt']; ?>">
+                <?php endif;
+                ?>
+            </div>
+            <div class="col-lg-6">
+                <div class="subtitle">
+                    <?php the_field('aboutus_title_2'); ?>
+                </div>
+                <div class="aboutus__text">
+                    <?php the_field('aboutus_descr_2'); ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="subtitle">
+                    <?php the_field('aboutus_title_3'); ?>
+                </div>
+                <div class="aboutus__text">
+                    <?php the_field('aboutus_descr_3'); ?>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <?php // подставляем изображение (вариант с массивом) из группы полей
+                $image = get_field('aboutus_img_3');
+
+                if (!empty($image)): ?>
+                    <img class="aboutus__img"
+                         src="<?php echo $image['url']; ?>"
+                         alt="<?php echo $image['alt']; ?>">
+                <?php endif;
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+get_footer();
+?>
--- page-team.php
+++ page-team.php
@@ -8,4 +8,25 @@ Template Name: Наша команда
     get_heager();
 ?>
 
+<div class="specialists">
+    <div class="container">
+        <div class="title"><?php the_field('team_title'); ?></div>
+        <div class="row">
+            <div class="col-lg-10 offset-lg-1">
+                <?php // подставляем изображение (вариант с массивом) из группы полей
+                $image = get_field('team_img');
+
+                if (!empty($image)): ?>
+                    <img class="specialists__img"
+                         src="<?php echo $image['url']; ?>"
+                         alt="<?php echo $image['alt']; ?>">
+                <?php endif;
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php
+    get_footer();
+?>
